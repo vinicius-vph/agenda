6cac3e17c7fe663a661e5dd9f4b8b74c2872ba1a
Vinicius Santos <vinicius.vph@gmail.com>
2021-08-23 21:02:25 -0300

    add modal base component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+import ReactModal from "react-modal";
+
+interface IContact {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+interface IModalProps {
+  children: any;
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+const Modal: React.FC<IModalProps> = ({ children, isOpen, setIsOpen }) => {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(()=> {
+    setModalStatus(isOpen);
+  },[isOpen])
+
+  return (
+    <ReactModal
+      className='bg_modal'
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          width: '432px',
+          height: '342px',
+          margin: '170px 167px 23px 486px',
+          padding: '16px 0',
+          borderRadius: '16px',
+          boxShadow: '0 16px 10px 0 rgba(0, 0, 0, 0.16)',
+          backgroundColor: 'var(--white-two)',
+        },
+        overlay: {
+          width: '1440px',
+          height: '1024px',
+          padding: '0 0 324px',
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
