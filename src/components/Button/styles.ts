a7d870cd19367aad26d6823faaf6b59ee9669ff6
Vinicius Santos <vinicius.vph@gmail.com>
2021-08-24 18:46:44 -0300

    add button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  display: inline;
+  width: 72px;
+  height: 32px;
+  margin: 15px 16px 16px 0;
+  padding: 8px 16px;
+  opacity: 0.32;
+  border-radius: 16px;
+  box-shadow: inset 0 0 0 1px rgba(255, 
+    255, 255, 0.16), 0 0 0 0.5px rgba(0, 
+      0, 0, 0.08), inset 0 0 0 0.5px rgba(0, 
+        0, 0, 0.08), 0 2px 4px 0.5px rgba(0, 0, 0, 0.16);
+  background-color: #fa7268;
+
+  .Salvar {
+    width: 40px;
+    height: 16px;
+    opacity: 0.32;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: var(--white-two);
+  }
+`;
